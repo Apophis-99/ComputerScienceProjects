e7c0f26e37a01a37503ef0c24623a998f1c31fcb
AS-99-Parkinson <Apophis-99@users.noreply.github.com>
2022-10-13 22:23:04 +0100

    ConAscii added

---
+++ ConAscii/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ConAscii;
+
+public static class Program
+{
+    private static byte GetInput()
+    {
+        while (true)
+        {
+            Console.Write("Enter a number between 0 and 255: ");
+            try
+            {
+                var num = Convert.ToByte(Console.ReadLine());
+                return num;
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("You must enter a number between 1 and 255!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+    }
+    
+    public static void Main()
+    {
+        while (true)
+        {
+            var num = GetInput();
+            Console.WriteLine($"{ num }: { Convert.ToChar(num) }");
+        }
+    }
+}
